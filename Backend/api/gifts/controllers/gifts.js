6fb7f32d937db477b278b9125cb2c56393f44604
Nada-Badreddine <badreddine.nada@gmail.com>
2022-04-23 19:32:09 +0200

    'conf'

--- Backend/api/gifts/controllers/gifts.js
+++ Backend/api/gifts/controllers/gifts.js
@@ -4,5 +4,10 @@
  * Read the documentation (https://strapi.io/documentation/developer-docs/latest/development/backend-customization.html#core-controllers)
  * to customize this controller
  */
+ 
+module.exports = {
 
-module.exports = {};
+   
+
+    
+};
