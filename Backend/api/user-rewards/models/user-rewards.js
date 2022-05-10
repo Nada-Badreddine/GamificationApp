da6c97f35a25b1dc99f0c02b88f73ad4824011f4
Nada-Badreddine <badreddine.nada@gmail.com>
2022-05-10 14:42:13 +0200

    oragansier

--- Backend/api/user-rewards/models/user-rewards.js
+++ Backend/api/user-rewards/models/user-rewards.js
@@ -1,10 +1,5 @@
 'use strict';
 
-/**
- * Read the documentation (https://strapi.io/documentation/developer-docs/latest/development/backend-customization.html#lifecycle-hooks)
- * to customize this model
- */
-
  var axios = require('axios');
  var data = JSON.stringify({
    "channel": "C03CQ5MEAAH",
@@ -37,7 +32,8 @@ module.exports = {
               
               var config = {
                 method: 'post',
-                url: 'https://hooks.slack.com/services/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',
+                url: 'https://hooks.slack.com/services/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',
+                
                 headers: { 
                   'Authorization': 'Bearer xoxp-xapp-1-A03DMG1QRLG-3452505188785-a0c3f60f6776e3f1276ce5b88f2a6cc9c0689ac2dd9281fb9701013a315afbb3', 
                   'Content-Type': 'application/json'
@@ -45,8 +41,7 @@ module.exports = {
                 data : data
                 
               };
-        console.log('aaaaaaaaaaaaa',result?.users_permissions_users[0]?.username) 
-            console.log('hhhhhhhhhhhhhhhhhhhhhhh', result);
+  
             axios(config)
             .then(function (response) {
               console.log('message sended successssssss');
